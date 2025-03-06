01c9d9af12de4836663fcf0837d63973f23e65f4
steeeee3 <stecarl03@gmail.com>
2025-03-06 23:06:06 +0100

    Added and tested new methods for ShipBoard

--- src/main/java/it/polimi/it/galaxytrucker/managers/ShipBoard.java
+++ src/main/java/it/polimi/it/galaxytrucker/managers/ShipBoard.java
@@ -4,17 +4,21 @@ import it.polimi.it.galaxytrucker.componenttiles.ComponentTile;
 import it.polimi.it.galaxytrucker.managers.exceptions.IllegalComponentPositionException;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Set;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+//TODO:
+//*  -metodi privati
+//*  -countExposedConnectors()
 public class ShipBoard {
     private List<List<Optional<ComponentTile>>> tileMatrix;
-    private Map<ComponentTile, List<Integer>> componentTilesPosition;
+    private Map<Class<? extends ComponentTile>, Set<List<Integer>>> componentTilesPosition;
 
     public ShipBoard() {
         final int COLUMNS = 7;
@@ -27,17 +31,119 @@ public class ShipBoard {
             )
             .collect(Collectors.toList());
 
-        this.componentTilesPosition = new HashMap<ComponentTile, List<Integer>>();
+        this.componentTilesPosition = new HashMap<>();
     }
 
+    /**
+     * Retrieves the entire board as a matrix of component tiles.
+     *
+     * @return A {@code List<List<Optional<ComponentTile>>>} representing the board.
+     *         Each inner list corresponds to a row, and each {@code Optional<ComponentTile>} 
+     *         represents a cell that may contain a component or be empty.
+     */
     public List<List<Optional<ComponentTile>>> getBoard() {
         return this.tileMatrix;
     }
 
-    public void addComponentTile (int row, int column, ComponentTile component) throws IllegalComponentPositionException {
-        if (this.tileMatrix.get(row).get(column).isPresent()) throw new IllegalComponentPositionException("You have already placed a component here");
+    /**
+     * Retrieves the component located at the specified position on the board.
+     *
+     * @param row    The row index of the component.
+     * @param column The column index of the component.
+     * @return An {@code Optional<ComponentTile>} containing the component if present,
+     *         or {@code Optional.empty()} if the cell is empty.
+     * @throws IndexOutOfBoundsException If {@code row} or {@code column} are out of bounds.
+     */
+    public Optional<ComponentTile> getComponent(int row, int column) {
+        return this.tileMatrix.get(row).get(column);
+    }
+
+    /**
+     * Retrieves all positions of components of the specified type on the board.
+     *
+     * @param componentType The class type of the component to search for.
+     * @return A {@code Set<List<Integer>>} containing the coordinates of all components 
+     *         of the given type, where each coordinate is represented as {@code List<Integer> [row, column]}.
+     *         Returns {@code null} if the component type is not found.
+     */
+    public Set<List<Integer>> getAllComponentsPositionOfType(Class<? extends ComponentTile> componentType) {
+        return this.componentTilesPosition.get(componentType);
+    }
+
+    /**
+     * Adds a component to the specified position on the board.
+     *
+     * @param row       The row index where the component should be placed.
+     * @param column    The column index where the component should be placed.
+     * @param component The {@code ComponentTile} to be added.
+     * @throws IllegalComponentPositionException If a component is already present at the specified position.
+     */
+    public void addComponentTile(int row, int column, ComponentTile component) throws IllegalComponentPositionException {
+        if (this.getComponent(row, column).isPresent()) throw new IllegalComponentPositionException("You have already placed a component here");
 
         this.tileMatrix.get(row).set(column, Optional.of(component));
-        this.componentTilesPosition.put(component, new ArrayList<>(List.of(row, column)));
+        this.componentTilesPosition.putIfAbsent(component.getClass(), new HashSet<>());
+        this.componentTilesPosition.get(component.getClass()).add(List.of(row, column));
+    }
+
+    /**
+     * Removes the component from the specified position on the board.
+     *
+     * @param row    The row index of the component to be removed.
+     * @param column The column index of the component to be removed.
+     * @throws IllegalComponentPositionException If there is no component at the specified position.
+     */
+    public void removeComponentTile(int row, int column) throws IllegalComponentPositionException {
+        if (this.getComponent(row, column).isEmpty()) throw new IllegalComponentPositionException("There is no element here");
+
+        Class<? extends ComponentTile> componentType = this.getComponent(row, column).get().getClass();
+
+        Set<List<Integer>> positions = this.componentTilesPosition.get(componentType);
+
+        this.tileMatrix.get(row).set(column, Optional.<ComponentTile>empty());
+
+        positions.removeIf(coord -> coord.equals(List.of(row, column)));
+        if (positions.isEmpty()) {
+            this.componentTilesPosition.remove(componentType);
+        }
+    }
+
+    /**
+     * Retrieves the neighboring components of the specified position on the board.
+     * The neighbors are returned in the following order: 
+     * <ul>
+     *   <li>Right component</li>
+     *   <li>Upper component</li>
+     *   <li>Left component</li>
+     *   <li>Lower component</li>
+     * </ul>
+     *
+     * @param row    The row index of the target component.
+     * @param column The column index of the target component.
+     * @return A {@code List<Optional<ComponentTile>>} containing the neighboring components.
+     *         Each element in the list is {@code Optional.empty()} if the corresponding neighbor does not exist.
+     */
+    public List<Optional<ComponentTile>> getNeighbourComponents(int row, int column) {
+        List<Optional<ComponentTile>> neighbours = new ArrayList<>();
+        //right component
+        neighbours.add(column + 1 >= this.tileMatrix.get(row).size() ? Optional.<ComponentTile>empty() : this.getComponent(row, column + 1));
+        //up component
+        neighbours.add(row - 1 < 0 ? Optional.<ComponentTile>empty() : this.getComponent(row - 1, column));
+        //left component
+        neighbours.add(column - 1 < 0 ? Optional.<ComponentTile>empty() : this.getComponent(row, column - 1));
+        //down component
+        neighbours.add(row + 1 >= this.tileMatrix.size() ? Optional.<ComponentTile>empty() : this.getComponent(row + 1, column));
+        return neighbours;
+    }
+
+    //? TESTING ONLY
+    public void printBoard() {
+        for (int i = 0; i < this.tileMatrix.size(); i++) {
+            for (int j = 0; j < this.tileMatrix.get(i).size(); j++) {
+                this.getComponent(i, j).ifPresentOrElse(_ -> System.out.print("[X]"), () -> System.out.print("[ ]"));
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
--- src/test/java/managers/ShipBoardTest.java
+++ src/test/java/managers/ShipBoardTest.java
@@ -3,10 +3,18 @@ package managers;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 
 import it.polimi.it.galaxytrucker.componenttiles.ComponentTile;
+import it.polimi.it.galaxytrucker.componenttiles.SingleCannon;
+import it.polimi.it.galaxytrucker.componenttiles.SingleEngine;
+import it.polimi.it.galaxytrucker.componenttiles.StructuralModule;
+import it.polimi.it.galaxytrucker.componenttiles.TileEdge;
 import it.polimi.it.galaxytrucker.managers.ShipBoard;
+import it.polimi.it.galaxytrucker.managers.exceptions.IllegalComponentPositionException;
 
 public class ShipBoardTest {
 
@@ -22,12 +30,140 @@ public class ShipBoardTest {
     void correctShipBoardTest() {
         ShipBoard ship = new ShipBoard();
 
-        for (int i = 0; i < ship.getBoard().size(); i++) {
-            for (int j = 0; j < ship.getBoard().get(i).size(); j++) {
-                assertEquals(ship.getBoard().get(i).get(j), Optional.<ComponentTile>empty());
-                System.out.print("[ ]");
-            }
-            System.out.println();
-        }
+        ship.printBoard();
+    }
+
+    @Test
+    void getAllComponentsPositionOfTypeTest() throws IllegalComponentPositionException {
+        ShipBoard ship = new ShipBoard();
+        ComponentTile cannon1 = new SingleCannon(TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE);
+        ComponentTile cannon2 = new SingleCannon(TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE);
+        ComponentTile engine = new SingleEngine(TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE);
+        ComponentTile structuralModule = new StructuralModule(TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE);
+        
+        ship.addComponentTile(0, 0, cannon1);
+        ship.addComponentTile(1, 1, cannon2);
+        ship.addComponentTile(2, 2, engine);
+        ship.addComponentTile(3, 3, structuralModule);
+
+        assertEquals(new HashSet<>(List.of(List.of(0, 0), List.of(1, 1))), 
+             ship.getAllComponentsPositionOfType(cannon1.getClass()));
+
+        assertEquals(new HashSet<>(List.of(List.of(2, 2))), 
+             ship.getAllComponentsPositionOfType(engine.getClass()));
+
+        assertEquals(new HashSet<>(List.of(List.of(3, 3))), 
+             ship.getAllComponentsPositionOfType(structuralModule.getClass()));
+    }
+
+    @Test
+    void addComponentTest() throws IllegalComponentPositionException {
+        ShipBoard ship = new ShipBoard();
+        ComponentTile cannon = new SingleCannon(TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE);
+        List<Integer> position = List.of(0,2);
+
+        ship.addComponentTile(position.get(0), position.get(1), cannon);
+
+        assertTrue(ship.getComponent(position.get(0), position.get(1)).isPresent());
+        assertEquals(ship.getComponent(position.get(0), position.get(1)).get(), cannon);
+
+        assertTrue(ship.getAllComponentsPositionOfType(cannon.getClass()).contains(position));
+    }
+
+    @Test
+    void IllegalAddPositionTest() throws IllegalComponentPositionException {
+        ShipBoard ship = new ShipBoard();
+        ComponentTile cannon = new SingleCannon(TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE);
+        List<Integer> position = List.of(0,2);
+
+        ship.addComponentTile(position.get(0), position.get(1), cannon);
+
+        assertThrows(IllegalComponentPositionException.class, () -> ship.addComponentTile(position.get(0), position.get(1), cannon));
+    }
+
+    @Test
+    void removeComponentTileTest() throws IllegalComponentPositionException {
+        ShipBoard ship = new ShipBoard();
+        ComponentTile cannon = new SingleCannon(TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE);
+        List<Integer> position = List.of(0,2);
+
+        ship.addComponentTile(position.get(0), position.get(1), cannon);
+        ship.addComponentTile(position.get(0), position.get(1) + 1, cannon);
+        ship.printBoard();
+
+        ship.removeComponentTile(position.get(0), position.get(1));
+        ship.printBoard();
+
+        assertTrue(ship.getComponent(position.get(0), position.get(1)).isEmpty());
+        assertTrue(!ship.getAllComponentsPositionOfType(cannon.getClass()).contains(position));
+    }
+
+    @Test
+    void illegalRemovePositionTest() {
+        ShipBoard ship = new ShipBoard();
+        List<Integer> position = List.of(0,2);
+
+        assertThrows(IllegalComponentPositionException.class, () -> ship.removeComponentTile(position.get(0), position.get(1)));
+    }
+
+    @Test
+    void getAllNeighbourComponentsTest() throws IllegalComponentPositionException {
+        ShipBoard ship = new ShipBoard();
+        ComponentTile cannon = new SingleCannon(TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE);
+        List<Integer> position = List.of(2,2);
+
+        //component
+        ship.addComponentTile(position.get(0), position.get(1), cannon);
+        //right component
+        ship.addComponentTile(position.get(0), position.get(1) + 1, cannon);
+        //up component
+        ship.addComponentTile(position.get(0) - 1, position.get(1), cannon);
+        //left component
+        ship.addComponentTile(position.get(0), position.get(1) - 1, cannon);
+        //down component
+        ship.addComponentTile(position.get(0) + 1, position.get(1), cannon);
+
+        List<Optional<ComponentTile>> result = ship.getNeighbourComponents(position.get(0), position.get(1));
+
+        assertIterableEquals(List.of(Optional.of(cannon), Optional.of(cannon), Optional.of(cannon), Optional.of(cannon)), result);
+    }
+
+    @Test
+    void getNeighbourComponentsWithBlanksTest() throws IllegalComponentPositionException {
+        ShipBoard ship = new ShipBoard();
+        ComponentTile cannon = new SingleCannon(TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE);
+        List<Integer> position = List.of(2,2);
+
+        //component
+        ship.addComponentTile(position.get(0), position.get(1), cannon);
+        //right component is empty
+        //up component
+        ship.addComponentTile(position.get(0) - 1, position.get(1), cannon);
+        //left component
+        ship.addComponentTile(position.get(0), position.get(1) - 1, cannon);
+        //down component
+        ship.addComponentTile(position.get(0) + 1, position.get(1), cannon);
+
+        List<Optional<ComponentTile>> result = ship.getNeighbourComponents(position.get(0), position.get(1));
+
+        assertIterableEquals(List.of(Optional.<ComponentTile>empty(), Optional.of(cannon), Optional.of(cannon), Optional.of(cannon)), result);
+    }
+
+    @Test
+    void getNeighbourComponentsWithAllBlanksTest() throws IllegalComponentPositionException {
+        ShipBoard ship = new ShipBoard();
+        ComponentTile cannon = new SingleCannon(TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE, TileEdge.SINGLE);
+        List<Integer> position = List.of(2,2);
+
+        //component
+        ship.addComponentTile(position.get(0), position.get(1), cannon);
+        //right component is empty
+        //up component is empty
+        //left component is empty
+        //down component is empty
+
+        List<Optional<ComponentTile>> result = ship.getNeighbourComponents(position.get(0), position.get(1));
+
+        assertIterableEquals(List.of(Optional.<ComponentTile>empty(), Optional.<ComponentTile>empty(), Optional.<ComponentTile>empty(), Optional.<ComponentTile>empty()), result);
     }
 }
