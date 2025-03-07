5364b305b2d501a7c736e7aa00ab0798e9fb3f62
Serbol2003 <serbol003@gmail.com>
2025-03-07 18:56:01 +0100

    mod

--- src/main/java/it/polimi/it/galaxytrucker/gameStates/GameTurn.java
+++ src/main/java/it/polimi/it/galaxytrucker/gameStates/GameTurn.java
@@ -1,7 +1,5 @@
 package it.polimi.it.galaxytrucker.gameStates;
 
-import it.polimi.it.galaxytrucker.aventurecard.AdventureCard;
-
 public interface GameTurn {
     AdventureCard drawAdventureCard();
     void playerPartecipate(int playerID);
