40af6966309895cc886165ce96e67c44777a0eb1
Giacomo Amaduci <giacomo.amaducci31@gmail.com>
2025-03-07 12:13:44 +0100

    Added EmptyTile

---
+++ src/main/java/it/polimi/it/galaxytrucker/componenttiles/EmptyTile.java
@@ -0,0 +1,7 @@
+package it.polimi.it.galaxytrucker.componenttiles;
+
+public class EmptyTile extends ComponentTile {
+    public EmptyTile() {
+        super(TileEdge.SMOOTH, TileEdge.SMOOTH, TileEdge.SMOOTH, TileEdge.SMOOTH);
+    }
+}
