41d18a17d04c5d748c05fbb1a6be82e20829f6b4
ThomasBnc <thomas.bianconi@mail.polimi.it>
2025-03-31 19:46:36 +0200

    Add cardStates

---
+++ src/main/java/it/polimi/it/galaxytrucker/cardStates/EnginePowerState.java
@@ -0,0 +1,22 @@
+package it.polimi.it.galaxytrucker.cardStates;
+
+import it.polimi.it.galaxytrucker.design.statePattern.State;
+import it.polimi.it.galaxytrucker.design.statePattern.StateMachine;
+
+public class EnginePowerState extends State {
+
+    @Override
+    public void enter(StateMachine fsm) {
+
+    }
+
+    @Override
+    public void update(StateMachine fsm) {
+
+    }
+
+    @Override
+    public void exit(StateMachine fsm) {
+
+    }
+}
